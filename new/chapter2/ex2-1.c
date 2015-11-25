5281ebe463f2b4a96d40c2346ff27f7412abf5be
shiyue <shiyue@shiyuedeMacBook-Pro.local>
2015-11-25 14:37:07 +0800

    c第二章练习

---
+++ new/chapter2/ex2-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void print(void);
+
+int main(int argc, char const *argv[]) {
+
+  print();
+  return 0;
+}
+void print(void)
+{
+  printf("Anton Bruckner\n");
+  printf("Anton\nBruckner\n");
+  printf("Anton");
+  printf(" Bruckner\n");
+}
