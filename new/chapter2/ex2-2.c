6780c96cc57e98c7291ae1043d0c0aee58ecf248
shiyue <shiyue@shiyuedeMacBook-Pro.local>
2015-11-25 14:39:40 +0800

    简单输出函数

---
+++ new/chapter2/ex2-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void print(void);
+
+int main(int argc, char const *argv[])
+{
+  print();
+  return 0;
+}
+void print(void)
+{
+  printf("My name is shiyue, I live in Beijing\n");
+}
