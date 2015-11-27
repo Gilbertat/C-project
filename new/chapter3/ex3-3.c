75b602b8e227f5d5de6cb6cf1b809aa9b9461783
shiyue <shiyue@shiyuedeMacBook-Pro.local>
2015-11-27 13:46:57 +0800

    转义字符使用

---
+++ new/chapter3/ex3-3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void print(void);
+
+int main(int argc, char const *argv[])
+{
+  print();
+  return 0;
+}
+
+void print(void)
+{
+  printf("\a Startled by the sudden sound,Sally shouted,\"By the Great Pumpkin, what was that!\"\n");
+}
